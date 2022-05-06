5bdd150ec5c717366db5139ace2447521ae0c922
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-06 15:35:55 +0600

    create electronics server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running electronics wirehouse server')
+});
+app.listen(port, () => {
+    console.log('Listening to port', port)
+})
