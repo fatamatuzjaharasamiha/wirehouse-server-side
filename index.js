ceeecd71cee906af0a1ff69552cf446dcdaaacc6
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-07 01:59:02 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.me6tg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('site connected')
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
     res.send('Running electronics wirehouse server')
