8c66d193bbfe0ee4409a7a78179710a0cb0afb94
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-07 03:38:19 +0600

    load all products

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -14,15 +14,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.me6tg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const productCollection = client.db('electronicsHouse').collection('product')
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray()
+            res.send(products);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running electronics wirehouse server')
